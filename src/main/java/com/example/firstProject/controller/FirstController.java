93802610b5f9d39ed630f9036f666a6352f4eb71
jiyeon <jiyeonyun43@gmail.com>
2022-05-15 22:16:29 +0900

    add MVC

---
+++ src/main/java/com/example/firstProject/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.example.firstProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username","지연");
+        return "greeting"; //templates/greetings.mustache->브라우저로 전송
+    }
+
+}
