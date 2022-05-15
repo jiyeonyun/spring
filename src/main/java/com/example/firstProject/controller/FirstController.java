0ede7eda6b8fd45bb142614b4c63a4e15ad00e0d
jiyeon <jiyeonyun43@gmail.com>
2022-05-15 22:25:31 +0900

    add bye templete

--- src/main/java/com/example/firstProject/controller/FirstController.java
+++ src/main/java/com/example/firstProject/controller/FirstController.java
@@ -10,5 +10,9 @@ public class FirstController {
         model.addAttribute("username","지연");
         return "greeting"; //templates/greetings.mustache->브라우저로 전송
     }
-
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("username","지연");
+        return "bye";
+    }
 }
